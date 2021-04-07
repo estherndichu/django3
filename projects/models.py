b761984c9f13d3fe75f7eb2f2c316e813d1b4c8a
estherndichu <itskuijenga@gmail.com>
2021-04-07 11:44:00 +0300

    add models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import cloudinary
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Projects(models.Model):
+    projects = models.URLField()
+
+    def __str__(self):
+        return self.projects
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = CloudinaryField('picture')
+    bio = models.CharField()
+    projects = models.ForeignKey(Projects,on_delete=models.CASCADE)
+    contact = models.EmailField()
+
+    def __str__(self):
+        return self.user
+
+def create_profile(sender,instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+
+post_save.connect(create_profile, sender = User)        

2a10d8b66fe0369db6bf441f5a0d9f3a526eda3a
estherndichu <itskuijenga@gmail.com>
2021-04-07 12:59:14 +0300

    authentication

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
-
+@login_required
 def index(request):
     
-    return render(request,'index.html',)
+    return render(request,'projects/index.html',)

ac5bb730ada2777a265e3702cca8b1c963a92603
estherndichu <itskuijenga@gmail.com>
2021-04-08 10:36:05 +0300

    registration redirect

--- awards/urls.py
+++ awards/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib.auth import views 
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
+    path('accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', views.logout_then_login, {"next_page": '/'}),

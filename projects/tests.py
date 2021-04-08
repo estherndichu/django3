01adf6e1969b6149d47375587dce6af5f87ca1de
estherndichu <itskuijenga@gmail.com>
2021-04-08 10:55:08 +0300

    test project

--- projects/tests.py
+++ projects/tests.py
@@ -6,10 +6,10 @@ class ProfileTestClass(TestCase):
 
     def setUp(self):
         # Creating a new editor and saving it
-        self.njenga= User(id=1, username='njengaen', password='beau2019')
-        self.njenga.save_user()
+        self.user= User(id=1, username='njengaen', password='beau2019')
+        self.user.save_user()
 
-        self.post= Site(title = 'Test Post',userprofile = self.njenga)
+        self.project= Project(title = 'Test Project',userprofile = self.njenga)
         self.post.save()
 
     def test_instance(self):
@@ -17,4 +17,22 @@ class ProfileTestClass(TestCase):
 
     def tearDown(self):
         Profile.objects.all().delete()
-        Project.objects.all().delete()
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='njenga')
+        self.project = Project.objects.create(id=1, project='http://njenga.com', description='wtwhthstshstshst', title='test project')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_project(self):
+        self.project.save_project()
+        project = Project.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_projects(self):
+        self.project.save()
+        projects = Project.objects.all()
+        self.assertTrue(len(posts) > 0)
+
